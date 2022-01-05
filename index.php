dd61e8921ff8f1c959443518460bb6d2e7f436fd
Rayyouu <rayane.ainseba@ynov.com>
2022-01-05 20:51:19 +0100

    add basic file

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+$mysqli = new mysqli("localhost", "root", "", "php_exam"); // Connexion à la db "php_exam"
+// si vous avez une erreur ici, remplacez le deuxième "root" par une string vide
+
+var_dump($result);
+?>
