ce76dd63c6fdf7b1ccf2e5b9ae50100c4a52b46d
Rayyouu <rayane.ainseba@ynov.com>
2022-01-06 23:33:33 +0100

    add basic file and architecture of project

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
+// require('functions.php');
 $mysqli = new mysqli("localhost", "root", "", "php_exam"); // Connexion à la db "php_exam"
 // si vous avez une erreur ici, remplacez le deuxième "root" par une string vide
 
-var_dump($result);
+// $element = [14, 56, 45, "4\n"];
 ?>
